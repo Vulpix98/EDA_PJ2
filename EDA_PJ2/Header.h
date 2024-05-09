5c1870902c3814867ed7962b79ffa2eefec803af
Vulpix98 <leonelqo@hotmail.com>
2024-05-09 11:28:35 +0100

    definir estruturas

--- EDA_PJ2/Header.h
+++ EDA_PJ2/Header.h
@@ -1,1 +1,13 @@
-#pragma once
+// structs
+typedef struct registo
+{
+    int valor;
+    struct registo* seguinte;
+} Aresta;
+
+typedef struct registo2
+{
+    int vertice;
+    Aresta* aresta;
+    struct registo2* seguinte;
+} Grafo;
