9cc50dbdc2c5077848c5c537ed40c3ece59074d2
ItzYerRob <yerrobofficial@gmail.com>
2024-05-26 16:05:46 +0100

    SomaRemoveVert

--- EDA_PJ2/Source.c
+++ EDA_PJ2/Source.c
@@ -31,6 +31,7 @@ void main()
 void menu(Grafo* grafo)
 {
 	int option;
+	int maxSum;
 
 	do
 	{
@@ -39,6 +40,8 @@ void menu(Grafo* grafo)
 		printf("2- Listar\n");
 		printf("3- Procura em profundidade!\n");
 		printf("4- Soma dos valores dado um caminho!\n");
+		printf("5- Selecionar vertice\n");
+		printf("6- Encontrar soma maxima\n");
 		printf("0- Sair\n\n");
 		printf("Opcao: ");
 		scanf("%d", &option);
@@ -99,6 +102,13 @@ void menu(Grafo* grafo)
 			}
 			break;
 		}
+		case 5:
+			grafo = selectVertexMenu(grafo);
+			break;
+		case 6:
+			maxSum = findMaxSum(grafo);
+			//printf("Maximum sum without repeating x/y coordinates: %d\n", maxSum);
+			break;
 			
 		default:
 			printf("Valor invalido");
@@ -240,6 +250,251 @@ Grafo* deleteEdge(Grafo* grafo)
 	return (grafo);
 }
 
+
+
+Grafo* selectVertexMenu(Grafo* grafo)
+{
+	int x, y;
+	int subOption;
+
+	printf("Digite as coordenadas do vertice (x y): ");
+	scanf("%d %d", &x, &y);
+
+	Grafo* vertice = grafo;
+
+	// Find the vertex x, y
+	while (vertice != NULL && (vertice->x != x || vertice->y != y)) {
+		vertice = vertice->seguinte;
+	}
+
+	if (vertice == NULL) {
+		printf("Vertice nao encontrado.\n");
+		return grafo;
+	}
+
+	do {
+		printf("Vertice selecionado (%d, %d)\n", x, y);
+		printf("1- Remover vertice\n");
+		printf("2- Remover aresta\n");
+		printf("0- Voltar\n\n");
+		printf("Opcao: ");
+		scanf("%d", &subOption);
+
+		switch (subOption)
+		{
+		case 0:
+			return grafo;
+
+		case 1:
+			grafo = removeVertex(grafo, x, y);
+			return grafo;
+
+		case 2:
+			grafo = RemoveSingleEdge(grafo, x, y);
+			break;
+
+		default:
+			printf("Valor invalido");
+			break;
+		}
+	} while (subOption != 0);
+
+	return grafo;
+}
+
+
+Grafo* RemoveSingleEdge(Grafo* grafo, int x, int y) {
+	Grafo* current = grafo;
+
+	// Find the vertex x, y
+	while (current != NULL && (current->x != x || current->y != y)) {
+		current = current->seguinte;
+	}
+
+	if (current == NULL) {
+		printf("Vertice nao encontrado.\n");
+		return grafo;
+	}
+
+	printf("Arestas conectadas ao vertice (%d, %d): ", x, y);
+	Aresta* aresta = current->aresta;
+	while (aresta != NULL) {
+		printf("%d ", aresta->valor);
+		aresta = aresta->seguinte;
+	}
+	printf("\n");
+
+	int edgeToRemove;
+	printf("Digite o valor da aresta a ser removida: ");
+	scanf("%d", &edgeToRemove);
+
+	// Remove the edge from the origin vertex
+	Aresta* prevAresta = NULL;
+	aresta = current->aresta;
+	while (aresta != NULL) {
+		if (aresta->valor == edgeToRemove) {
+			if (prevAresta == NULL) {
+				current->aresta = aresta->seguinte;
+			}
+			else {
+				prevAresta->seguinte = aresta->seguinte;
+			}
+			free(aresta);
+			printf("Aresta removida com sucesso.\n");
+			break;
+		}
+		prevAresta = aresta;
+		aresta = aresta->seguinte;
+	}
+
+	// Now remove the corresponding edge from the destination vertex
+	Grafo* destVertex = grafo;
+	while (destVertex != NULL && destVertex->vertice != edgeToRemove) {
+		destVertex = destVertex->seguinte;
+	}
+
+	if (destVertex != NULL) {
+		Aresta* destAresta = destVertex->aresta;
+		Aresta* prevDestAresta = NULL;
+		while (destAresta != NULL) {
+			if (destAresta->valor == current->vertice) {
+				if (prevDestAresta == NULL) {
+					destVertex->aresta = destAresta->seguinte;
+				}
+				else {
+					prevDestAresta->seguinte = destAresta->seguinte;
+				}
+				free(destAresta);
+				printf("Aresta correspondente removida do vertice destino.\n");
+				break;
+			}
+			prevDestAresta = destAresta;
+			destAresta = destAresta->seguinte;
+		}
+	}
+
+	return grafo;
+}
+
+
+Grafo* removeVertex(Grafo* grafo, int x, int y) {
+	if (grafo == NULL) {
+		return NULL;
+	}
+
+	Grafo* current = grafo;
+	Grafo* prev = NULL;
+
+	// Find the vertex to be removed
+	while (current != NULL && (current->x != x || current->y != y)) {
+		prev = current;
+		current = current->seguinte;
+	}
+
+	// If the vertex is not found, return the graph as is
+	if (current == NULL) {
+		return grafo;
+	}
+
+	// Remove all edges pointing to this vertex
+	Grafo* temp = grafo;
+	while (temp != NULL) {
+		Aresta* aresta = temp->aresta;
+		Aresta* prevAresta = NULL;
+		while (aresta != NULL) {
+			if (aresta->valor == current->vertice) {
+				if (prevAresta == NULL) {
+					temp->aresta = aresta->seguinte;
+				}
+				else {
+					prevAresta->seguinte = aresta->seguinte;
+				}
+				free(aresta);
+				break;
+			}
+			prevAresta = aresta;
+			aresta = aresta->seguinte;
+		}
+		temp = temp->seguinte;
+	}
+
+	// Remove the vertex and its edges
+	if (prev == NULL) {
+		grafo = current->seguinte;
+	}
+	else {
+		prev->seguinte = current->seguinte;
+	}
+
+	Aresta* aresta = current->aresta;
+	while (aresta != NULL) {
+		Aresta* tempAresta = aresta;
+		aresta = aresta->seguinte;
+		free(tempAresta);
+	}
+
+	free(current);
+	return grafo;
+}
+
+void maxSumRecursive(Grafo* node, int* xSelected, int* ySelected, int currentSum, int* maxSum, int* path, int* maxPath, int depth, int* maxDepth) {
+	if (node == NULL) {
+		if (currentSum > *maxSum) {
+			*maxSum = currentSum;
+			*maxDepth = depth;
+			memcpy(maxPath, path, depth * sizeof(int));
+		}
+		return;
+	}
+
+	// Recursive call without selecting the current node
+	maxSumRecursive(node->seguinte, xSelected, ySelected, currentSum, maxSum, path, maxPath, depth, maxDepth);
+
+	// Recursive call with selecting the current node if x and y are not selected
+	if (!xSelected[node->x] && !ySelected[node->y]) {
+		xSelected[node->x] = 1;
+		ySelected[node->y] = 1;
+		path[depth] = node->vertice; // Add current node to the path
+		maxSumRecursive(node->seguinte, xSelected, ySelected, currentSum + node->vertice, maxSum, path, maxPath, depth + 1, maxDepth);
+		xSelected[node->x] = 0;
+		ySelected[node->y] = 0;
+	}
+}
+
+int findMaxSum(Grafo* grafo) {
+	int maxSum = 0;
+	int maxDepth = 0;
+
+	// This assumes the graph can have up to 100 nodes on x and y axis
+	int* xSelected = (int*)calloc(100, sizeof(int));
+	int* ySelected = (int*)calloc(100, sizeof(int));
+
+	// Allocate space for paths
+	int* path = (int*)malloc(100 * sizeof(int)); // Assume max path length is 100
+	int* maxPath = (int*)malloc(100 * sizeof(int));
+
+	if (xSelected == NULL || ySelected == NULL || path == NULL || maxPath == NULL) {
+		fprintf(stderr, "Memory allocation failed\n");
+		exit(1);
+	}
+
+	maxSumRecursive(grafo, xSelected, ySelected, 0, &maxSum, path, maxPath, 0, &maxDepth);
+
+	// Print the maximum sum and the path
+	printf("Maximum sum without repeating x/y coordinates: %d\n", maxSum);
+	printf("Path taken: ");
+	for (int i = 0; i < maxDepth; i++) {
+		printf("%d ", maxPath[i]);
+	}
+	printf("\n\n");
+
+	free(xSelected);
+	free(ySelected);
+	free(path);
+	free(maxPath);
+
+	return maxSum;
+}
 
 
 // insert all the Edge to their respective Vertex
